f1b0dbf5fce9e47ef89f4377425cdd6fba93440b
Avelexis <alsydoruk642@gmail.com>
2026-09-03 20:50:15 +0200

    try

--- src/server.js
+++ src/server.js
@@ -8,7 +8,13 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(pinoHttp());
+app.use(
+  pinoHttp({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
